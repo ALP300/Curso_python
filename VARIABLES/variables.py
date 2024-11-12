2b8008d7b04185105beab6aa982166257ffd6f76
ALP300 <aitorpalomino1@gmail.com>
2024-11-11 21:56:42 -0500

    Realizar variables

---
+++ VARIABLES/variables.py
@@ -0,0 +1,14 @@
+texto= input("Por favor, ingresa un texto: ")
+numero1= int(input("Por favor, ingresa un número1: "))
+numero2= int(input("Por favor, ingresa un número2: "))
+suma= numero1+numero2
+resta= numero1-numero2
+mult= numero1*numero2
+div= numero1/numero2
+print("El texto es: ",texto)
+print("El número 1 es: ",numero1)
+print("El número 2 es: ",numero2)
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es: ", mult)
+print("La división es: ", div)
