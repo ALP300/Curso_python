48a926caee242bc3b1e48766d7fe2b784065b83f
ALP300 <aitorpalomino1@gmail.com>
2024-11-16 21:06:42 -0500

    Realizar ejercicio 2

---
+++ EJERCICIO-SEMANA1/ejercicio2.py
@@ -0,0 +1,19 @@
+'''
+Escribe un programa que solicite al usuario un número entero y calcule
+su cuadrado y su cubo.
+-------------------------------------------------------------------------------------------------------
+numero= int(input("Ingresa un número entero: "))
+cuadrado= numero**2
+cubo= numero**3
+print(cuadrado)
+print(cubo)
+
+'''
+import math
+
+numero= int(input("Ingresa un número entero"))
+cuadrado= math.pow(numero,2)
+cubo= math.pow(numero,3)
+print(cuadrado)
+print(1e5)
+print(cubo)
